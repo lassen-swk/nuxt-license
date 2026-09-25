882604a667f10a84ce412e2e59536d2448201ebd
lassen-swk <c.lassen@schwarzwaldklinik.health>
2026-09-25 08:23:31 +0200

    baselog

--- app/composables/api-fetch.ts
+++ app/composables/api-fetch.ts
@@ -5,6 +5,7 @@ export function useAppFetch<T = unknown>(
   const  { authCookie } = useUser();
   const config = useRuntimeConfig();
   const baseUrl = import.meta.server ? config.apiBase : config.public.apiBase;
+  console.log(config.apiBase, config.public.apiBase)
 
   return $fetch<T>(`${baseUrl}${endpoint}`, {
     ...options,
